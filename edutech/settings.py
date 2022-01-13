457cb2217e6891b2ac2854a5fcab66a59ca50906
Ivan Bok <68679411+ivanbok@users.noreply.github.com>
2022-01-13 16:42:10 +0800

    Update settings.py

--- edutech/settings.py
+++ edutech/settings.py
@@ -20,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-jzer3ulnovio4le385b_lnolh63%d&dk5-uq=0wdm7p6n^q(5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
